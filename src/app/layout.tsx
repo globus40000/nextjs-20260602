7b88ccebf788536816f381d34462ca677f096980
Pavel Ashifin <globus40000@gmail.com>
2026-06-09 16:17:01 +0600

    chore: setup basic project

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import { FC, PropsWithChildren } from "react";
+import "./globals.css";
+
+const RootLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">Tailwind работает!</h1>
+    </div>
+  );
+};
+
+export default Home;
