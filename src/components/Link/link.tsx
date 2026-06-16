1ebced6225e4c1aee500cc3e4b7a509caf6a581b
Pavel Ashifin <globus40000@gmail.com>
2026-06-16 16:53:02 +0600

    feat(link): add prop highlightIfActive

--- src/components/Link/link.tsx
+++ src/components/Link/link.tsx
@@ -1,13 +1,34 @@
+"use client";
+
+import clsx from "clsx";
 import NextLink from "next/link";
+import { usePathname } from "next/navigation";
 import { ComponentProps, FC } from "react";
 
-export const Link: FC<ComponentProps<typeof NextLink>> = ({
+type LinkProps = ComponentProps<typeof NextLink> & {
+  highlightIfActive?: boolean;
+};
+
+export const Link: FC<LinkProps> = ({
+  href,
   prefetch = false,
+  highlightIfActive = false,
+  className,
   children,
   ...rest
 }) => {
+  const pathname = usePathname();
+  const isLinkActive = href === pathname;
+
   return (
-    <NextLink prefetch={prefetch} {...rest}>
+    <NextLink
+      href={href}
+      prefetch={prefetch}
+      className={clsx(className, {
+        "text-orange-400": highlightIfActive && isLinkActive,
+      })}
+      {...rest}
+    >
       {children}
     </NextLink>
   );
