92eff4feecdc6484ba0eb030f338afdb9e424623
Pavel Ashifin <globus40000@gmail.com>
2026-06-16 14:12:22 +0600

    style(footer): add y margins

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const Footer: FC = () => {
   return (
-    <div className="text-xs text-gray-500">
+    <div className="text-xs text-gray-500 mt-4 mb-3">
       &copy; {new Date().getFullYear()} Tennis Store. All rights reserved.
     </div>
   );
