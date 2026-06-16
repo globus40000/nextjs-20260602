7aa9fa7dee9f359f8c0f7cc282370aaf77645b84
Pavel Ashifin <globus40000@gmail.com>
2026-06-16 19:19:13 +0600

    feat: implement Header component

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,5 +1,16 @@
 import { FC } from "react";
+import { Logo } from "@/components/Logo/logo";
+import { Navigation } from "@/components/Navigation/navigation";
 
 export const Header: FC = () => {
-  return <div>Header</div>;
+  return (
+    <div className="py-4 flex items-center relative">
+      <div className="grow justify-items-center">
+        <Logo />
+      </div>
+      <div className="absolute right-0">
+        <Navigation />
+      </div>
+    </div>
+  );
 };
