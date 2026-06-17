31d8fc65f2626d39b2b495c30473e1bb411098e9
Pavel Ashifin <globus40000@gmail.com>
2026-06-17 23:21:57 +0600

    feat: add ProductCard component

---
+++ src/components/ProductCard/product-card.tsx
@@ -0,0 +1,28 @@
+import { Link } from "@/components/Link/link";
+import { Product } from "@/types/products";
+import Image from "next/image";
+import { FC } from "react";
+
+type ProductCardProps = {
+  product: Product;
+};
+
+export const ProductCard: FC<ProductCardProps> = ({ product }) => {
+  const { id, imageUrl, name } = product;
+
+  return (
+    <div>
+      <Link href={`/product/${id}`}>
+        <Image
+          src={imageUrl}
+          alt={name}
+          width={440}
+          height={560}
+          className="object-cover aspect-440/560 border rounded-lg border-gray-200 shadow-xs"
+          loading="eager"
+        />
+        <h3>{name}</h3>
+      </Link>
+    </div>
+  );
+};
