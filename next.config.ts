0bc87cdf49c99e41f2fc64efd6518f85e6f6f641
Pavel Ashifin <globus40000@gmail.com>
2026-06-17 23:20:55 +0600

    chore(next): configure image remote patterns and allow local IP in dev

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    dangerouslyAllowLocalIP: process.env.NODE_ENV === "development",
+    remotePatterns: [new URL("http://localhost:4000/**")],
+  },
 };
 
 export default nextConfig;
