d6a6a334351725bb37ce73f89c6d7f17fd8daf55
Pavel Ashifin <globus40000@gmail.com>
2026-06-26 19:46:47 +0600

    feat(home): add products slider and seo heading to homepage

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,11 +1,21 @@
-import { Link } from "@/components/Link/link";
+import { LinkAll } from "@/components/LinkAll/link-all";
+import { ProductsSlider } from "@/components/ProductsSlider/products-slider";
+import { rackets } from "@/mocks/rackets";
 import { FC } from "react";
 
 const HomePage: FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-blue-500 gap-4">
-      <h1 className="text-4xl font-bold text-white">Home page</h1>
-      <Link href="/rackets">Rackets</Link>
+    <div>
+      <h1 className="sr-only">
+        Tennis Store — интернет-магазин теннисной экипировки
+      </h1>
+      <section>
+        <div className="flex items-baseline justify-between mb-9">
+          <h2>Ракетки</h2>
+          <LinkAll productType="racket" />
+        </div>
+        <ProductsSlider products={rackets.slice(0, 10)} />
+      </section>
     </div>
   );
 };
