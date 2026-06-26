696bcc1a633f121e3f66431521ed84ce726b2958
Pavel Ashifin <globus40000@gmail.com>
2026-06-26 16:57:05 +0600

    refactor(product-card): forward image loading and preload strategies

--- src/components/ProductCard/product-card.tsx
+++ src/components/ProductCard/product-card.tsx
@@ -1,13 +1,19 @@
 import { Link } from "@/components/Link/link";
 import { Product } from "@/types/products";
 import Image from "next/image";
-import { FC } from "react";
+import { ComponentProps, FC } from "react";
+
+type NextImageProps = ComponentProps<typeof Image>;
 
 type ProductCardProps = {
   product: Product;
-};
+} & Pick<NextImageProps, "loading" | "preload">;
 
-export const ProductCard: FC<ProductCardProps> = ({ product }) => {
+export const ProductCard: FC<ProductCardProps> = ({
+  product,
+  loading,
+  preload,
+}) => {
   const { id, imageUrl, name } = product;
 
   return (
@@ -19,7 +25,8 @@ export const ProductCard: FC<ProductCardProps> = ({ product }) => {
           width={440}
           height={560}
           className="object-cover aspect-440/560 border rounded-lg border-gray-200 shadow-xs"
-          loading="eager"
+          loading={loading}
+          preload={preload}
         />
         <h3 className="text-gray-700 text-sm mt-4">{name}</h3>
       </Link>
