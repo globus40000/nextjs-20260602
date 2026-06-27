9c4e799a7c0f31eefece91c65705a3de02223bad
Pavel Ashifin <globus40000@gmail.com>
2026-06-28 00:37:24 +0600

    feat: add generateStaticParams

--- src/app/(app)/product/[id]/page.tsx
+++ src/app/(app)/product/[id]/page.tsx
@@ -1,6 +1,10 @@
 import { ProductContainer } from "@/components/Product/product-container";
 import { FC } from "react";
 
+export const generateStaticParams = () => {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+};
+
 const ProductPage: FC<PageProps<"/product/[id]">> = async ({ params }) => {
   const { id } = await params;
 
