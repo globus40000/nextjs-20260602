3bb9dd3462ff05f80ba21ce6bb4cb2ab77e9d273
Pavel Ashifin <globus40000@gmail.com>
2026-06-29 15:26:13 +0600

    feat(services): add query params support to getProducts service

--- src/services/get-products.ts
+++ src/services/get-products.ts
@@ -2,8 +2,21 @@ import { BASE_API_URL } from "@/constants/api";
 import { Response } from "@/types/api";
 import { type Product } from "@/types/products";
 
-export const getProducts = async (): Response<Product[]> => {
-  const result = await fetch(`${BASE_API_URL}/products`);
+type Params = {
+  page?: number;
+  limit?: number;
+};
+
+export const getProducts = async (params: Params = {}): Response<Product[]> => {
+  const url = new URL(`${BASE_API_URL}/products`);
+
+  Object.entries(params).forEach(([key, value]) => {
+    if (value !== undefined) {
+      url.searchParams.append(key, String(value));
+    }
+  });
+
+  const result = await fetch(url);
 
   if (result.status === 404) {
     return { isError: false, data: undefined };
