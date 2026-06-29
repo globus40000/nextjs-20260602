4b3aeb66aa7f93711953e196fbdabba476ec7b4e
Pavel Ashifin <globus40000@gmail.com>
2026-06-29 17:08:32 +0600

    refactor(brand-filter): fallback to empty array on API error instead of crashing

--- src/components/BrandFilter/brand-filter-container.tsx
+++ src/components/BrandFilter/brand-filter-container.tsx
@@ -1,17 +1,9 @@
 import { BrandFilter } from "@/components/BrandFilter/brand-filter";
 import { getBrands } from "@/services/get-brands";
-import { notFound } from "next/navigation";
 import { FC } from "react";
 
 export const BrandFilterContainer: FC = async () => {
-  const { isError, data: brands } = await getBrands();
-
-  if (isError) {
-    throw new Error("Something went wrong");
-  }
-  if (!brands) {
-    return notFound();
-  }
+  const { data: brands = [] } = await getBrands();
 
   return <BrandFilter brands={brands} />;
 };
