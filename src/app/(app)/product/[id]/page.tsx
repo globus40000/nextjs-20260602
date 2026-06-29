91ee948ddfe391262e715082936a333833578cf1
Pavel Ashifin <globus40000@gmail.com>
2026-06-29 14:50:33 +0600

    feat(product): add suspense loading for product page

--- src/app/(app)/product/[id]/page.tsx
+++ src/app/(app)/product/[id]/page.tsx
@@ -1,5 +1,6 @@
+import { Loading } from "@/components/Loading/loading";
 import { ProductContainer } from "@/components/Product/product-container";
-import { FC } from "react";
+import { FC, Suspense } from "react";
 
 export const generateStaticParams = () => {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
@@ -11,7 +12,9 @@ const ProductPage: FC<PageProps<"/product/[id]">> = async ({ params }) => {
   return (
     <div>
       <article>
-        <ProductContainer id={parseInt(id)} />
+        <Suspense fallback={<Loading />}>
+          <ProductContainer id={parseInt(id)} />
+        </Suspense>
       </article>
     </div>
   );
