07b05ce6289ffe8e98b46a27d132853425135ba6
Pavel Ashifin <globus40000@gmail.com>
2026-06-29 15:09:16 +0600

    feat(components): fetch products data from api in ProductsListContainer

--- src/components/ProductsList/products-list-container.tsx
+++ src/components/ProductsList/products-list-container.tsx
@@ -1,7 +1,17 @@
 import { ProductsList } from "@/components/ProductsList/products-list";
-import { rackets } from "@/mocks/rackets";
+import { getProducts } from "@/services/get-products";
+import { notFound } from "next/navigation";
 import { FC } from "react";
 
-export const ProductsListContainer: FC = () => {
-  return <ProductsList products={rackets} />;
+export const ProductsListContainer: FC = async () => {
+  const { isError, data: products } = await getProducts();
+
+  if (isError) {
+    throw new Error("Something went wrong");
+  }
+  if (!products) {
+    return notFound();
+  }
+
+  return <ProductsList products={products} />;
 };
