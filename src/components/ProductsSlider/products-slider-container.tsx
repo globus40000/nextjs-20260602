263a40ea937649558d72db3587696fd8fa23d500
Pavel Ashifin <globus40000@gmail.com>
2026-06-29 15:34:29 +0600

    feat(components): fetch products data from api in ProductsSliderContainer

--- src/components/ProductsSlider/products-slider-container.tsx
+++ src/components/ProductsSlider/products-slider-container.tsx
@@ -1,7 +1,17 @@
 import { ProductsSlider } from "@/components/ProductsSlider/products-slider";
-import { rackets } from "@/mocks/rackets";
+import { getProducts } from "@/services/get-products";
+import { notFound } from "next/navigation";
 import { FC } from "react";
 
-export const ProductsSliderContainer: FC = () => {
-  return <ProductsSlider products={rackets.slice(0, 10)} />;
+export const ProductsSliderContainer: FC = async () => {
+  const { isError, data: products } = await getProducts({ page: 1, limit: 10 });
+
+  if (isError) {
+    throw new Error("Something went wrong");
+  }
+  if (!products) {
+    return notFound();
+  }
+
+  return <ProductsSlider products={products} />;
 };
