dd4adf1b4858e62c92779b0338b4a727636aa8ca
Pavel Ashifin <globus40000@gmail.com>
2026-06-29 16:42:37 +0600

    feat(components): fetch brands data from api in BrandFilterContainer

--- src/components/BrandFilter/brand-filter-container.tsx
+++ src/components/BrandFilter/brand-filter-container.tsx
@@ -1,7 +1,17 @@
 import { BrandFilter } from "@/components/BrandFilter/brand-filter";
-import { brands } from "@/mocks/brands";
+import { getBrands } from "@/services/get-brands";
+import { notFound } from "next/navigation";
 import { FC } from "react";
 
-export const BrandFilterContainer: FC = () => {
+export const BrandFilterContainer: FC = async () => {
+  const { isError, data: brands } = await getBrands();
+
+  if (isError) {
+    throw new Error("Something went wrong");
+  }
+  if (!brands) {
+    return notFound();
+  }
+
   return <BrandFilter brands={brands} />;
 };
