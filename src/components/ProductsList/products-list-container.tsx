c614d749859eb72a2211bd46bfec20fbf227b687
Pavel Ashifin <globus40000@gmail.com>
2026-06-29 16:14:32 +0600

    feat(products): add top10 mode support to ProductsListContainer

--- src/components/ProductsList/products-list-container.tsx
+++ src/components/ProductsList/products-list-container.tsx
@@ -1,10 +1,17 @@
 import { ProductsList } from "@/components/ProductsList/products-list";
 import { getProducts } from "@/services/get-products";
+import { getProductsTop10 } from "@/services/get-products-top-10";
 import { notFound } from "next/navigation";
 import { FC } from "react";
 
-export const ProductsListContainer: FC = async () => {
-  const { isError, data: products } = await getProducts({ page: 1, limit: 20 });
+type Props = {
+  top10?: boolean;
+};
+
+export const ProductsListContainer: FC<Props> = async ({ top10 = false }) => {
+  const { isError, data: products } = top10
+    ? await getProductsTop10()
+    : await getProducts({ page: 1, limit: 20 });
 
   if (isError) {
     throw new Error("Something went wrong");
