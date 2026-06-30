4d8686bac4dab8b70177d9ae38e2f12c60489ee6
Pavel Ashifin <globus40000@gmail.com>
2026-07-01 01:53:55 +0600

    feat(auth): connect login form to server action

--- src/components/LoginForm/login-form.tsx
+++ src/components/LoginForm/login-form.tsx
@@ -1,14 +1,41 @@
+"use client";
+
 import { FormInput } from "@/components/FormInput/form-input";
 import { SubmitButton } from "@/components/SubmitButton/submit-button";
-import { FC } from "react";
+import { loginAction } from "@/server-actions/login-action";
+import { FC, useActionState, useEffect } from "react";
 
 export const LoginForm: FC = () => {
+  const [state, formAction, isPending] = useActionState(loginAction, {});
+  const { error, fields, redirectTo } = state;
+
+  useEffect(() => {
+    if (redirectTo) {
+      window.location.assign(redirectTo);
+    }
+  }, [redirectTo]);
+
   return (
     <div>
-      <form className="flex flex-col gap-6">
-        <FormInput type="text" name="login" label="Login" required />
-        <FormInput type="password" name="password" label="Password" required />
-        <SubmitButton />
+      <form className="flex flex-col gap-6" action={formAction}>
+        <FormInput
+          type="text"
+          name="login"
+          label="Login"
+          required
+          disabled={isPending}
+          defaultValue={fields?.login ?? ""}
+        />
+        <FormInput
+          type="password"
+          name="password"
+          label="Password"
+          required
+          disabled={isPending}
+          defaultValue={fields?.password ?? ""}
+        />
+        <SubmitButton disabled={isPending} isLoading={isPending} />
+        {error && <div className="text-red-400">{error}</div>}
       </form>
     </div>
   );
