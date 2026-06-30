5881edebac642e96793e475c7170ff957b9fd1a4
Pavel Ashifin <globus40000@gmail.com>
2026-06-30 16:42:57 +0600

    feat(layout): add NextTopLoader to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import { FC, PropsWithChildren } from "react";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin", "cyrillic"] });
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextTopLoader height={2} color="#51A2FF" />
+        {children}
+      </body>
     </html>
   );
 };
