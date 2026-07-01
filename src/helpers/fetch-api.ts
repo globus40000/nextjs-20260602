f59133603090601c15bfe242c0d02c319d33ab85
Pavel Ashifin <globus40000@gmail.com>
2026-07-02 01:47:33 +0600

    feat(api): add skipResponseBody option to fetchApi helper

--- src/helpers/fetch-api.ts
+++ src/helpers/fetch-api.ts
@@ -2,15 +2,19 @@ import { ApiResponse } from "@/types/api";
 
 export async function fetchApi<T>(
   url: string | URL,
-  options?: RequestInit,
+  options?: RequestInit & { skipResponseBody?: boolean },
 ): ApiResponse<T> {
-  const response = await fetch(url, options);
+  const { skipResponseBody, ...fetchOptions } = options || {};
+  const response = await fetch(url, fetchOptions);
 
   if (response.status === 404) {
     return { isError: false, data: undefined, response };
   }
   if (!response.ok) {
     return { isError: true, data: undefined, response };
+  }
+  if (skipResponseBody) {
+    return { isError: false, data: undefined, response };
   }
 
   const data = await response.json();
