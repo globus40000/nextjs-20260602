c2849c487efdd10de5440faadfae8c92c6b35182
Pavel Ashifin <globus40000@gmail.com>
2026-07-01 21:34:12 +0600

    feat(pages): add logout page

---
+++ src/app/(app)/(auth)/logout/page.tsx
@@ -0,0 +1,12 @@
+import { LogoutButton } from "@/components/LogoutButton/logout-button";
+import { FC } from "react";
+
+const LogoutPage: FC = () => {
+  return (
+    <div>
+      <LogoutButton />
+    </div>
+  );
+};
+
+export default LogoutPage;
