596c0e244894e6fe8da99deff44ed518624ad323
Pavel Ashifin <globus40000@gmail.com>
2026-07-02 01:52:58 +0600

    feat(auth): add metadata to logout page

--- src/app/(app)/(auth)/logout/page.tsx
+++ src/app/(app)/(auth)/logout/page.tsx
@@ -1,5 +1,14 @@
 import { LogoutButton } from "@/components/common/LogoutButton/logout-button";
+import { Metadata } from "next";
 import { FC } from "react";
+
+export const metadata: Metadata = {
+  title: "Выход – TENNIS STORE",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 const LogoutPage: FC = () => {
   return (
