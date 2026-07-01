d59b51e5ba65db533694f511e5dc7d0f15d651d8
Pavel Ashifin <globus40000@gmail.com>
2026-07-02 00:37:06 +0600

    feat(api): include native response object in ApiResponse

--- src/helpers/fetch-api.ts
+++ src/helpers/fetch-api.ts
@@ -7,13 +7,13 @@ export async function fetchApi<T>(
   const response = await fetch(url, options);
 
   if (response.status === 404) {
-    return { isError: false, data: undefined };
+    return { isError: false, data: undefined, response };
   }
   if (!response.ok) {
-    return { isError: true, data: undefined };
+    return { isError: true, data: undefined, response };
   }
 
   const data = await response.json();
 
-  return { isError: false, data };
+  return { isError: false, data, response };
 }
