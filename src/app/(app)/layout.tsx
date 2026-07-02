b0db0348578f149f9ede9f6ec04383e77a0287ae
Pavel Ashifin <globus40000@gmail.com>
2026-07-03 00:44:08 +0600

    feat(auth): wrap app layout with AuthContextProvider and hydrate user data

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,18 +1,24 @@
 import { FC, PropsWithChildren } from "react";
 import { Header } from "@/components/common/Header/header";
 import { Footer } from "@/components/common/Footer/footer";
+import { getCurrentUser } from "@/services/auth/get-current-user";
+import { AuthContextProvider } from "@/providers/AuthContextProvider/auth-context-provider";
 
-export const Layout: FC<PropsWithChildren> = ({ children }) => {
+export const Layout: FC<PropsWithChildren> = async ({ children }) => {
+  const { data: user } = await getCurrentUser();
+
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="px-5 border-b border-gray-200">
-        <Header />
-      </header>
-      <main className="px-5 py-13 grow">{children}</main>
-      <footer className="px-5 border-t border-gray-200">
-        <Footer />
-      </footer>
-    </div>
+    <AuthContextProvider user={user}>
+      <div className="min-h-screen flex flex-col">
+        <header className="px-5 border-b border-gray-200">
+          <Header />
+        </header>
+        <main className="px-5 py-13 grow">{children}</main>
+        <footer className="px-5 border-t border-gray-200">
+          <Footer />
+        </footer>
+      </div>
+    </AuthContextProvider>
   );
 };
 
