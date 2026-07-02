df419fe69da7a75372095e38aa18ade31eb18c1b
Pavel Ashifin <globus40000@gmail.com>
2026-07-03 00:47:04 +0600

    feat(navigation): toggle Login/Logout links based on auth state

--- src/components/common/Navigation/navigation.tsx
+++ src/components/common/Navigation/navigation.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import { Link } from "@/components/common/Link/link";
-import { FC } from "react";
+import { AuthContext } from "@/providers/AuthContextProvider/auth-context";
+import { FC, useContext } from "react";
 
 export const Navigation: FC = () => {
+  const { isAuthorized } = useContext(AuthContext);
+
   return (
     <nav>
       <ul className="flex gap-6 text-xs text-gray-700">
@@ -20,11 +25,19 @@ export const Navigation: FC = () => {
             Топ-10
           </Link>
         </li>
-        <li>
-          <Link href="/login" highlightIfActive>
-            Войти
-          </Link>
-        </li>
+        {isAuthorized ? (
+          <li>
+            <Link href="/logout" highlightIfActive>
+              Выйти
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <Link href="/login" highlightIfActive>
+              Войти
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
