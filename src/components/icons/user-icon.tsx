6324fe1718f2f8e120daa8ea2df3efcf620f0cf5
Pavel Ashifin <globus40000@gmail.com>
2026-07-03 20:46:45 +0600

    feat(icons): add UserIcon component

---
+++ src/components/icons/user-icon.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+
+type Props = {
+  className?: string;
+};
+
+export const UserIcon: FC<Props> = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://w3.org"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+      <circle cx="12" cy="7" r="4" />
+    </svg>
+  );
+};
