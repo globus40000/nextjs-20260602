154002183c555dca7c723e82354e01acfbb50e7d
Pavel Ashifin <globus40000@gmail.com>
2026-07-03 20:48:11 +0600

    feat(ui): add variants to Button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,11 +1,24 @@
+import clsx from "clsx";
 import { ComponentProps, FC } from "react";
 
-type Props = ComponentProps<"button">;
+type Props = ComponentProps<"button"> & {
+  variant?: "primary" | "secondary";
+};
 
-export const Button: FC<Props> = ({ children, ...props }) => {
+export const Button: FC<Props> = ({
+  children,
+  variant = "primary",
+  ...props
+}) => {
   return (
     <button
-      className="rounded-md bg-blue-600 px-4 py-2 text-sm font-semibold text-white cursor-pointer flex justify-center items-center gap-2 focus:outline-blue-800 disabled:cursor-not-allowed disabled:opacity-60"
+      className={clsx(
+        "rounded-md px-4 py-2 text-sm font-semibold cursor-pointer flex justify-center items-center gap-2 disabled:cursor-not-allowed disabled:opacity-60",
+        variant === "primary" &&
+          "bg-blue-600 focus:outline-blue-800 text-white",
+        variant === "secondary" &&
+          "bg-gray-100 focus:outline-gray-200 text-gray-700",
+      )}
       {...props}
     >
       {children}
