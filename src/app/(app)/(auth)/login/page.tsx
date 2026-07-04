7c101b0130723c9a0a8b768923f06a8908236e0b
Pavel Ashifin <globus40000@gmail.com>
2026-07-04 15:53:42 +0600

    feat(auth): add heading and signup link to login page

--- src/app/(app)/(auth)/login/page.tsx
+++ src/app/(app)/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@/components/common/Link/link";
 import { LoginForm } from "@/components/common/LoginForm/login-form";
 import { Metadata } from "next";
 import { FC } from "react";
@@ -12,8 +13,15 @@ export const metadata: Metadata = {
 
 const LoginPage: FC = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-medium mb-9">Вход</h1>
       <LoginForm />
+      <p className="text-sm text-gray-600 mt-8">
+        Впервые у нас?{" "}
+        <Link href="/signup" className="text-blue-500">
+          Создать аккаунт
+        </Link>
+      </p>
     </div>
   );
 };
