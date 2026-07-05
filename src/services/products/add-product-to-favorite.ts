8beb8b82fa0bfe484b6caa0ba73d57f1744f6198
Pavel Ashifin <globus40000@gmail.com>
2026-07-05 23:28:07 +0600

    refactor(services): pass cookies explicitly in addProductToFavorite for server context

--- src/services/products/add-product-to-favorite.ts
+++ src/services/products/add-product-to-favorite.ts
@@ -1,11 +1,16 @@
 import { BASE_API_URL } from "@/constants/api";
 import { fetchApi } from "@/helpers/fetch-api";
 import { Product } from "@/types/products";
+import { cookies } from "next/headers";
 
 export const addProductToFavorite = async (id: Product["id"]) => {
+  const cookiesStore = await cookies();
+
   return fetchApi<unknown>(`${BASE_API_URL}/product/${id}/favorite`, {
     method: "POST",
-    credentials: "include",
+    headers: {
+      Cookie: cookiesStore.toString(),
+    },
     skipResponseBody: true,
   });
 };
