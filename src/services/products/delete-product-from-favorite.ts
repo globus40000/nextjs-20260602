18ad736ae7f1fcfae40a1dd9dc02c95aa6e621b0
Pavel Ashifin <globus40000@gmail.com>
2026-07-05 23:29:49 +0600

    refactor(services): use next/headers cookies in deleteProductFromFavorite

--- src/services/products/delete-product-from-favorite.ts
+++ src/services/products/delete-product-from-favorite.ts
@@ -1,11 +1,16 @@
 import { BASE_API_URL } from "@/constants/api";
 import { fetchApi } from "@/helpers/fetch-api";
 import { Product } from "@/types/products";
+import { cookies } from "next/headers";
 
 export const deleteProductFromFavorite = async (id: Product["id"]) => {
+  const cookiesStore = await cookies();
+
   return fetchApi(`${BASE_API_URL}/product/${id}/favorite`, {
     method: "DELETE",
-    credentials: "include",
+    headers: {
+      Cookie: cookiesStore.toString(),
+    },
     skipResponseBody: true,
   });
 };
