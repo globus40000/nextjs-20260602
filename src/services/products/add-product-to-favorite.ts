13a99dae9afe9756d84831192dab1293509a672c
Pavel Ashifin <globus40000@gmail.com>
2026-07-07 01:11:36 +0600

    refactor(services): switch to credentials include in addProductToFavorite

--- src/services/products/add-product-to-favorite.ts
+++ src/services/products/add-product-to-favorite.ts
@@ -1,16 +1,11 @@
 import { BASE_API_URL } from "@/constants/api";
 import { fetchApi } from "@/helpers/fetch-api";
 import { Product } from "@/types/products";
-import { cookies } from "next/headers";
 
 export const addProductToFavorite = async (id: Product["id"]) => {
-  const cookiesStore = await cookies();
-
   return fetchApi<unknown>(`${BASE_API_URL}/product/${id}/favorite`, {
     method: "POST",
-    headers: {
-      Cookie: cookiesStore.toString(),
-    },
+    credentials: "include",
     skipResponseBody: true,
   });
 };
