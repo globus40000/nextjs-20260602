7bad125671dcd14c4efa63b4f084a555076ddbab
Pavel Ashifin <globus40000@gmail.com>
2026-07-07 01:11:59 +0600

    refactor(services): switch to credentials include in deleteProductFromFavorite

--- src/services/products/delete-product-from-favorite.ts
+++ src/services/products/delete-product-from-favorite.ts
@@ -1,16 +1,11 @@
 import { BASE_API_URL } from "@/constants/api";
 import { fetchApi } from "@/helpers/fetch-api";
 import { Product } from "@/types/products";
-import { cookies } from "next/headers";
 
 export const deleteProductFromFavorite = async (id: Product["id"]) => {
-  const cookiesStore = await cookies();
-
   return fetchApi<unknown>(`${BASE_API_URL}/product/${id}/favorite`, {
     method: "DELETE",
-    headers: {
-      Cookie: cookiesStore.toString(),
-    },
+    credentials: "include",
     skipResponseBody: true,
   });
 };
