3c95bc7e82347c163edfd55ff186b345f3e7a781
Pavel Ashifin <globus40000@gmail.com>
2026-07-07 01:05:25 +0600

    feat(layout): wrap app layout with FavoriteContextProvider

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,21 +3,24 @@ import { Header } from "@/components/common/Header/header";
 import { Footer } from "@/components/common/Footer/footer";
 import { getCurrentUser } from "@/services/auth/get-current-user";
 import { AuthContextProvider } from "@/providers/AuthContextProvider/auth-context-provider";
+import { FavoriteContextProvider } from "@/providers/FavoriteContextProvider/favorite-context-provider";
 
 export const Layout: FC<PropsWithChildren> = async ({ children }) => {
   const { data: user } = await getCurrentUser();
 
   return (
     <AuthContextProvider user={user}>
-      <div className="min-h-screen flex flex-col">
-        <header className="px-5 border-b border-gray-200">
-          <Header />
-        </header>
-        <main className="px-5 py-13 grow">{children}</main>
-        <footer className="px-5 border-t border-gray-200">
-          <Footer />
-        </footer>
-      </div>
+      <FavoriteContextProvider>
+        <div className="min-h-screen flex flex-col">
+          <header className="px-5 border-b border-gray-200">
+            <Header />
+          </header>
+          <main className="px-5 py-13 grow">{children}</main>
+          <footer className="px-5 border-t border-gray-200">
+            <Footer />
+          </footer>
+        </div>
+      </FavoriteContextProvider>
     </AuthContextProvider>
   );
 };
