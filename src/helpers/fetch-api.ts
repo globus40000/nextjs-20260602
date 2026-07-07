b132565f6c488f718ee82f37565231e7a1569c18
Pavel Ashifin <globus40000@gmail.com>
2026-07-07 20:20:52 +0600

    feat(helpers): support relative paths in fetchApi helper

--- src/helpers/fetch-api.ts
+++ src/helpers/fetch-api.ts
@@ -1,11 +1,17 @@
+import { BASE_API_URL } from "@/constants/api";
 import { ApiResponse } from "@/types/api";
 
 export async function fetchApi<T>(
   url: string | URL,
   options?: RequestInit & { skipResponseBody?: boolean },
 ): ApiResponse<T> {
+  const targetUrl =
+    typeof url === "string" && url.startsWith("/")
+      ? `${BASE_API_URL}${url}`
+      : url;
+
   const { skipResponseBody, ...fetchOptions } = options || {};
-  const response = await fetch(url, fetchOptions);
+  const response = await fetch(targetUrl, fetchOptions);
 
   if (response.status === 404) {
     return { isError: false, data: undefined, response };
