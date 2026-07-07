17d1c27666c7e60b63406a03daeccf0925334be2
Pavel Ashifin <globus40000@gmail.com>
2026-07-07 20:19:51 +0600

    feat(config): add API_ROUTES constant

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,20 @@
+import { Product } from "@/types/products";
+
 export const BASE_API_URL = "http://localhost:4000/api";
+
+export const API_ROUTES = {
+  auth: {
+    login: "/auth/login",
+    logout: "/auth/logout",
+    signup: "/auth/signup",
+    currentUser: "/auth/user",
+  },
+  products: {
+    list: "/products",
+    top10: "/top-10",
+    brands: "/brands",
+    byId: (id: Product["id"]) => `/product/${id}`,
+    metaById: (id: Product["id"]) => `/meta/product/${id}`,
+    favorite: (id: Product["id"]) => `/product/${id}/favorite`,
+  },
+} as const;
