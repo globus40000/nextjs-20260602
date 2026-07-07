4b2333c39a0e0278b3532c9dd98662c28630c4b8
Pavel Ashifin <globus40000@gmail.com>
2026-07-08 01:05:11 +0600

    feat(products): add ProductClientContainer with data fetching and error boundaries

---
+++ src/components/common/Product/product-client-container.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ErrorMessage } from "@/components/common/ErrorMessage/error-message";
+import { Loading } from "@/components/common/Loading/loading";
+import { NotFoundView } from "@/components/common/NotFoundView/not-found-view";
+import { Product } from "@/components/common/Product/product";
+import { useProductById } from "@/swr/products/useProductById";
+import { type Product as TypeProduct } from "@/types/products";
+import { FC } from "react";
+
+type Props = {
+  id: TypeProduct["id"];
+};
+
+export const ProductClientContainer: FC<Props> = ({ id }) => {
+  const { isLoading, error, data: product } = useProductById(id);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <ErrorMessage />;
+  }
+  if (!product) {
+    return <NotFoundView />;
+  }
+
+  return (
+    <div>
+      <Product product={product} />
+    </div>
+  );
+};
