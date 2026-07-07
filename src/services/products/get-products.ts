8fb85d1b724172cc95f2dce140c1939d9857ddab
Pavel Ashifin <globus40000@gmail.com>
2026-07-08 01:02:34 +0600

    refactor(services): use withParams helper in getProducts service

--- src/services/products/get-products.ts
+++ src/services/products/get-products.ts
@@ -1,5 +1,6 @@
-import { API_ROUTES, BASE_API_URL } from "@/constants/api";
+import { API_ROUTES } from "@/constants/api";
 import { fetchApi } from "@/helpers/fetch-api";
+import { withParams } from "@/helpers/with-params";
 import { type Product } from "@/types/products";
 import { cookies } from "next/headers";
 
@@ -9,17 +10,9 @@ type Params = {
 };
 
 export const getProducts = async (params: Params = {}) => {
-  const url = new URL(API_ROUTES.products.list, BASE_API_URL);
-
-  Object.entries(params).forEach(([key, value]) => {
-    if (value !== undefined) {
-      url.searchParams.append(key, String(value));
-    }
-  });
-
   const cookiesStore = await cookies();
 
-  return fetchApi<Product[]>(url, {
+  return fetchApi<Product[]>(withParams(API_ROUTES.products.list, params), {
     headers: {
       Cookie: cookiesStore.toString(),
     },
