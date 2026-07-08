9feca5e87f2589f0f2ff3210b24d5748f33e377c
Pavel Ashifin <globus40000@gmail.com>
2026-07-09 01:52:26 +0600

    feat(api): make response optional and add includeResponse option

--- src/helpers/fetch-api.ts
+++ src/helpers/fetch-api.ts
@@ -3,27 +3,31 @@ import { ApiResponse } from "@/types/api";
 
 export async function fetchApi<T>(
   url: string | URL,
-  options?: RequestInit & { skipResponseBody?: boolean },
+  options?: RequestInit & {
+    skipResponseBody?: boolean;
+    includeResponse?: boolean;
+  },
 ): ApiResponse<T> {
   const targetUrl =
     typeof url === "string" && url.startsWith("/")
       ? `${BASE_API_URL}${url}`
       : url;
 
-  const { skipResponseBody, ...fetchOptions } = options || {};
+  const { skipResponseBody, includeResponse, ...fetchOptions } = options || {};
   const response = await fetch(targetUrl, fetchOptions);
+  const extra = includeResponse ? { response } : {};
 
   if (response.status === 404) {
-    return { isError: false, data: undefined, response };
+    return { isError: false, data: undefined, ...extra };
   }
   if (!response.ok) {
-    return { isError: true, data: undefined, response };
+    return { isError: true, data: undefined, ...extra };
   }
   if (skipResponseBody) {
-    return { isError: false, data: undefined, response };
+    return { isError: false, data: undefined, ...extra };
   }
 
   const data = await response.json();
 
-  return { isError: false, data, response };
+  return { isError: false, data, ...extra };
 }
--- src/types/api.ts
+++ src/types/api.ts
@@ -1,5 +1,5 @@
 export type ApiResponse<Entity> = Promise<{
   isError: boolean;
   data?: Entity;
-  response: Response;
+  response?: Response;
 }>;
