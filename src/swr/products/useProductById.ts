60d8da83e39032e030c947d9eadbe876558a67bd
Pavel Ashifin <globus40000@gmail.com>
2026-07-09 02:41:02 +0600

    refactor(products): strongly type useProductById and add flexible data unpacking

--- src/swr/products/useProductById.ts
+++ src/swr/products/useProductById.ts
@@ -1,18 +1,37 @@
 import { API_ROUTES } from "@/constants/api";
 import { createSWRFetcher } from "@/helpers/create-swr-fetcher";
+import { getProductById } from "@/services/products/get-product-by-id";
 import { Product } from "@/types/products";
 import useSWR from "swr";
 
-const fetcher = createSWRFetcher<{ product: Product }>();
+type FetcherData = { product: Product };
+type ServiceData = Awaited<ReturnType<typeof getProductById>>;
+
+const fetcher = createSWRFetcher<FetcherData>();
 
 export const useProductById = (id: Product["id"]) => {
-  const result = useSWR(API_ROUTES.products.byId(id), fetcher, {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-  });
+  const result = useSWR<FetcherData | ServiceData | undefined>(
+    API_ROUTES.products.byId(id),
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+    },
+  );
 
-  return {
-    ...result,
-    data: result.data?.product,
-  };
+  let data;
+  let error = result.error;
+
+  if (result.data && "product" in result.data) {
+    data = result.data.product;
+  }
+  if (result.data && "data" in result.data) {
+    data = result.data.data;
+
+    if (!error && result.data.isError) {
+      error = "Something went wrong";
+    }
+  }
+
+  return { ...result, data, error };
 };
